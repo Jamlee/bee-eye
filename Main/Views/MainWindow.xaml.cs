7676e77ae0e1a11c236c8b06ef7ed9082bed83f9
jamlee@jamlee.cn <you@example.com>
2019-04-17 22:17:55 +0800

    update: adjust theme to white and update readme

--- Main/Views/MainWindow.xaml.cs
+++ Main/Views/MainWindow.xaml.cs
@@ -1,15 +1,24 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Bee.Eye.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+
+    private void NavRegion_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+      if (e.LeftButton == MouseButtonState.Pressed)
+      {
+        this.DragMove();
+      }
+    }
+  }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # bee-eye
 
-bee-eye is fast and simple game server control panel
+fast and simple game server control panel
 
 [![Build status](https://ci.appveyor.com/api/projects/status/y1ahn7y6j6r4vy8o?svg=true)](https://ci.appveyor.com/project/Jamlee/bee-eye)
 
